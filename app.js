3e98433a744c2f6a0f74d16d8c140b72f3e1fe61
Estellio <pirko.vichmann@voco.ee>
2022-11-11 14:27:48 +0200

    OOP-Books [Delivers #183779736]

--- app.js
+++ app.js
@@ -1,92 +1,33 @@
-document.addEventListener('DOMContentLoaded', getBooks)
-const booksTable = document.querySelector('table')
-const form = document.querySelector('form')
+ui = new UI()
+ls = new LS()
 
-booksTable.addEventListener('click', deleteBook)
-form.addEventListener('submit', addBooks)
+const submitButton = document.querySelector("#submit-book")
+const inputTitle = document.getElementById("title")
+const inputAuthor = document.getElementById("author")
+const inputISBN = document.getElementById("isbn")
+const table = document.querySelector("table")
 
-function addBooks () {
-    var table = document.getElementById("bookTable");
-    // Create an empty <tr> element and add it to the last position of the table
-    var row = table.insertRow(-1);
-    // Insert new cells (<td> elements) of the "new" <tr> element
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    // Identify input field values
-    let input1 = document.getElementById("title");
-    let input2 = document.getElementById("author");
-    let input3 = document.getElementById("isbn");
-    // Add text to the new cells
-    cell1.innerText = input1.value;
-    cell2.innerText = input2.value;
-    cell3.innerText = input3.value;
-    cell4.innerHTML = '<a href="#">X</a>'
-    addBookToLS([input1.value, input2.value, input3.value])
-    // Clear the input fields
-    input1.value = ''
-    input2.value = ''
-    input3.value = ''
+table.addEventListener("click", deleteBook)
+submitButton.addEventListener("click", addBook)
+document.addEventListener("DOMContentLoaded", getBooks)
+
+function addBook(e){
+    const book = new Book(inputTitle.value, inputAuthor.value, inputISBN.value)
+    console.log(book)
+    ui.addBook(book.title, book.author, book.isbn)
+    ls.addBook(book)
+    book.addedToUI()
+    e.preventDefault()
 }
 
 function deleteBook(e){
-    if(e.target.textContent == 'X'){
-        if(confirm('Are you sure you want to delete this book?')){
-            e.target.parentElement.parentElement.remove()
-            deleteBookLS(e.target.parentElement.parentElement.firstChild.textContent)
-        }
-    }
-}
-
-function addBookToLS (book) {
-    let books
-    if(localStorage.getItem('books') === null){
-        books = []
-    } else {
-        books = JSON.parse(localStorage.getItem('books'))
-    }
-    books.push(book)
-    localStorage.setItem('books', JSON.stringify(books))
-}
-
-function deleteBookLS(book) {
-    let books
-    if(localStorage.getItem('books') === null){
-        books = []
-    } else {
-        books = JSON.parse(localStorage.getItem('books'))
-    }
-    books.forEach((bookLS, bookIndex) => {
-        if(bookLS[0] === book){
-            books.splice(bookIndex, 1)
-        }
-    })
-    localStorage.setItem('books', JSON.stringify(books))
+    let book = e.target.parentElement.firstChild
+    ui.deleteBook(book)
+    book = e.target.parentElement.parentElement.children[2].innerText
+    ls.deleteBook(book)
 }
 
 function getBooks(){
-    let books
-    if(localStorage.getItem('books') === null){
-        books = []
-    } else {
-        books = JSON.parse(localStorage.getItem('books'))
-    }
-    books.forEach((book) => {
-        const tr = document.createElement('tr')
-        const td1 = document.createElement('td')
-        const td2 = document.createElement('td')
-        const td3 = document.createElement('td')
-        const td4 = document.createElement('td')
-        const a = document.createElement('a')
-        a.appendChild(document.createTextNode('X'))
-        a.setAttribute('href', '#')
-        td1.appendChild(document.createTextNode(book[0]))
-        td2.appendChild(document.createTextNode(book[1]))
-        td3.appendChild(document.createTextNode(book[2]))
-        td4.appendChild(a)
-        tr.append(td1, td2, td3, td4)
-        const tbody = document.querySelector('tbody')
-        tbody.append(tr)
-    })
+    let books = ls.getData("books")
+    ui.getBooks(books)
 }
---
+++ classes/book.js
@@ -0,0 +1,15 @@
+class Book{
+    constructor(title, author, isbn) {
+        this.title = title
+        this.author = author
+        this.isbn = isbn
+    }
+
+    addedToUI() {
+        console.log(`${this.title} was added to UI`)
+    }
+
+    addedToLS() {
+        console.log(`${this.title} was added to LS`)
+    }
+}
